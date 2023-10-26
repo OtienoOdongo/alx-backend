2079739cfd95acfb3d54f9040f52d98ebfa5ffd2
OtienoOdongo <otabiznake@gmail.com>
2023-10-26 16:41:14 +0000

    FIFO caching placement policy

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,22 +4,23 @@ using FIFO caching placement policy
 Its normally First in First out
 """
 
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
     """
     A caching system that implements
-    a FIFO (First-In-First-Out) algorithm.
+    FIFO (First-In-First-Out) algorithm
 
     Attributes:
         cache_data (dict):
-        A dictionary to store cached key-value pairs.
+         A dictionary to store cached key-value pairs.
     """
 
     def __init__(self):
         """
-        Initializes the FIFO cache by calling
+        Initialize the FIFO cache by calling
         the parent class's constructor.
         """
         super().__init__()
@@ -30,8 +31,8 @@ class FIFOCache(BaseCaching):
 
         If either the key or the item is None,
         this method does nothing.
-        If the number of items in the cache exceeds
-        the maximum allowed (MAX_ITEMS),
+        If the number of items in the cache
+        exceeds the maximum allowed (MAX_ITEMS),
         the method discards the first item
         added using FIFO and prints a discard message.
 
@@ -43,9 +44,9 @@ class FIFOCache(BaseCaching):
             pass
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
-            print(f"DISCARD: {first_key}")
-            del self.cache_data[first_key]
+            first_added_key = min(self.cache_data, key=self.cache_data.get)
+            self.cache_data.pop(first_added_key)
+            print(f'DISCARD: {first_added_key}')
 
         self.cache_data[key] = item
 
@@ -54,7 +55,7 @@ class FIFOCache(BaseCaching):
         Retrieve a cached item by its key.
 
         If the key is None or does not exist in the cache,
-        it returns None.
+         it returns None.
 
         Args:
             key:
@@ -64,6 +65,4 @@ class FIFOCache(BaseCaching):
             The cached item associated with the key,
             or None if the key is None or not found.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
