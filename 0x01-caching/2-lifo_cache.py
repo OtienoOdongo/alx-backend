0f14865675b75ec088beed9c49d7fe714f657dbb
OtienoOdongo <otabiznake@gmail.com>
2023-10-26 20:42:39 +0000

    LIFO cache placement

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -3,6 +3,7 @@
 LIFO (Last-In-First-Out) Cache Placement
 """
 
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -14,18 +15,16 @@ class LIFOCache(BaseCaching):
     Attributes:
         cache_data (dict):
             A dictionary to store cached key-value pairs.
-        custom_stack (list):
+        key_order (list):
             A list to maintain the order in
              which keys were added to the cache.
     """
-
     def __init__(self):
         """
         Initialize the LIFO cache by calling
         the parent class's constructor.
         """
         super().__init__()
-        self.custom_stack = []
 
     def put(self, key, item):
         """
@@ -35,34 +34,24 @@ class LIFOCache(BaseCaching):
             key (str): The key to be used for caching.
             item: The value to be cached.
         """
-        if key is None or item is None:
-            pass
-
-        if key in self.cache_data:
-            self.custom_stack.remove(key)
-
-        while len(self.custom_stack) >= BaseCaching.MAX_ITEMS:
-            discarded_key = self.custom_stack.pop()
-            self.cache_data.pop(discarded_key)
-            print(f'DISCARD: {discarded_key}')
-
-        self.custom_stack.append(key)
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.last_item)
+            print(f'DISCARD: {self.last_item}')
+        if key:
+            self.last_item = key
 
     def get(self, key):
         """
         Retrieve a cached item by its key.
 
-        If the key is None or does not exist in the cache,
-        it returns None.
-
         Args:
-            key (str):
-            The key to be used for retrieving the cached item.
+            key (str): The key to be used for retrieving the cached item.
 
         Returns:
-            The cached item associated with the key,
-             or None if the key is None or not found.
+            The cached item associated with the key
+            or None if the key is not found.
         """
         if key is None or key not in self.cache_data:
             return None
